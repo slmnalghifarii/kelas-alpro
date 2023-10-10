5512736f62992fe29a9762fbe9ac4a0f0a51cad0
slmnalghifarii <146912418+slmnalghifarii@users.noreply.github.com>
2023-10-10 21:16:19 +0700

    output.c

--- output.c
+++ output.c
@@ -2,7 +2,7 @@
 
 int main(){
     printf("Hello, ini adalah teks output\n");
-    printf("Nama saya %s\n", "Dian");
-    printf("Usia saya %d\n", 20);
+    printf("Nama saya %s\n", "Syifa");
+    printf("Usia saya %d\n", 18);
     return 0;
 }
